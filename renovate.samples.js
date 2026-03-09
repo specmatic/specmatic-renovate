97d95667ffbfcca70caca25c0aee83dc5d9107c8
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2026-03-09 16:31:59 +0530

    Add exclusion for specmatic/labs in renovate config

--- renovate.samples.js
+++ renovate.samples.js
@@ -7,6 +7,7 @@ module.exports = {
     "!specmatic/arazzo-paris-demo",
     "!specmatic/*iata*",
     "!specmatic/hooks-demo",
+    "!specmatic/labs",
     "!specmatic/specmatic-arazzo-ui-sample",
     "!specmatic/order-service-avro",
   ],
